53888f8daafff40e7a6486fe6baf96a037d575fc
Nreis280 <nicolasreissanto@gmail.com>
2023-08-18 07:20:48 -0300

    Criação de produto estocado

---
+++ src/main/java/br/com/fiap/domain/service/ProdutoestocadoService.java
@@ -0,0 +1,40 @@
+package br.com.fiap.domain.service;
+
+import br.com.fiap.domain.entity.ProdutoEstocado;
+import br.com.fiap.domain.repository.ProdutoEstocadoRepository;
+
+import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import java.util.Random;
+
+public class ProdutoestocadoService {
+
+    public static ProdutoEstocado entrada(ProdutoEstocado pe){
+        if (pe.getProduto().equals(null)) return null;
+        if (pe.getDeposito().equals(null)) return null;
+        pe.setEntrada(LocalDateTime.now());
+        pe.setSaida(null);
+        pe.setNumeroDeSerie(gerarNumeroDeSerie(pe));
+
+        return ProdutoEstocadoRepository.persist(pe);
+    }
+
+    private static String gerarNumeroDeSerie(ProdutoEstocado pe){
+        Random random = new Random();
+
+        var numeroAleatorio = random.nextInt(1000, 99999);
+
+        return numeroAleatorio + "" + gerarDigito(numeroAleatorio);
+
+    }
+
+    private static String gerarDigito(int numeroAleatorio){
+
+        var formatador = new DecimalFormat("00");
+        var valor = String.valueOf(numeroAleatorio).length() -2;
+        var n = valor >> 2;
+
+        return formatador.format(n);
+
+    }
+}
